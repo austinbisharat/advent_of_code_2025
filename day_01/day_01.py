83d2d3bdba9b7692b57e0ed2bfa91099a777cac6
Austin Bisharat <austinbisharat@gmail.com>
2025-11-30 22:04:00 -0800

    Solve Day 1

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,25 +1,55 @@
-from typing import TextIO
-from common.file_solver import FileSolver
+from types import NoneType
+from typing import Generic
+
+from common.line_solver import LineSolver, AbstractLineByLineSolution
 
 
-LoadedDataType = ...
+FileConfigType = None
+LineDataType = int
 
 
-def load(file: TextIO) -> LoadedDataType:
-    ...
+def parse_line(line: str) -> LineDataType:
+    prefix, remaining = line[0], int(line[1:])
+    return (int(prefix.lower() == "r") * 2 - 1) * remaining
 
 
-def solve_pt1(data: LoadedDataType) -> int:
-    ...
+class Part1Solution(AbstractLineByLineSolution[LineDataType, FileConfigType]):
+    def __init__(self) -> None:
+        self._dial_loc = 50
+        self._result = 0
+
+    def process_line(self, line: LineDataType) -> None:
+        self._dial_loc = (self._dial_loc + line) % 100
+        self._result += int(self._dial_loc == 0)
+
+    def result(self) -> int:
+        return self._result
 
 
-def solve_pt2(data: LoadedDataType) -> int:
-    return 0
+class Part2Solution(AbstractLineByLineSolution[LineDataType, FileConfigType]):
+    def __init__(self) -> None:
+        self._dial_loc = 50
+        self._result = 0
+
+    def process_line(self, line: LineDataType) -> None:
+        started_at_zero = self._dial_loc == 0
+        self._dial_loc += line
+        count_clicks = sum((
+            abs(self._dial_loc) // 100,
+            int(self._dial_loc < 0 and not started_at_zero),
+            int(self._dial_loc == 0),
+        ))
+        self._result += count_clicks
+        self._dial_loc = self._dial_loc % 100
+
+    def result(self) -> int:
+        return self._result
+
 
 
 if __name__ == "__main__":
-    FileSolver[LoadedDataType].construct_for_day(
+    LineSolver[LineDataType, FileConfigType].construct_for_day(
         day_number=1,
-        loader=load,
-        solutions=[solve_pt1, solve_pt2]
+        line_parser=parse_line,
+        solutions=[Part1Solution, Part2Solution],
     ).solve_all()
